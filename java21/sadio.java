b4f8774b0daec0368d402c667df9557560137929
jw13k <127805684+jw13k@users.noreply.github.com>
2023-11-02 22:33:21 +0900

    Update sadio.java

--- java21/sadio.java
+++ java21/sadio.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class WalkingTimeCalculator {
+public class WalkTCal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
